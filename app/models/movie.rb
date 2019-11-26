a365b124e10f0119e702384b6a1ce518acd3d7a5
Javi <javsteratii@gmail.com>
2019-11-26 11:03:01 -0500

    didnt finish

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,52 @@
 class Movie
+  #has many reviews
+  #has many viewers
   attr_accessor :title
 
   @@all = []
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
+
+  def title
+    @title
+  end 
+
+
+  def movie_select
+    #helper method that selects viewer == self from Review All array
+    Review.all.select{|movie| movie.movie == self}
+  end
+
+  def reviews 
+  #   - returns an array of all the `Review` instances for the `Movie`.
+    movie_select
+  end 
+
+  def reviewers
+  #   - returns an array of all of the `Viewer` instances that reviewed the `Movie`.
+    movie_select.map{|review| review.viewer}
+  end
+
+  def average_rating
+  #   - returns the average of all ratings for the `Movie` instance
+  #  add all ratings together and divide by the total number of ratings.
+    amount = movie_select.length * movie_select 
+    movie_select.map{|review| review.rating / amount}
+    #NOT FINISHED, NOT WORKING. SUBMITTED AS IS.
+  end 
+
+  def self. highest_rated
+  #   - returns the `Movie` instance with the highest average rating.
+  end 
+
+
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,50 @@
 class Viewer
+  #has many reviews
+  #has many movies 
   attr_accessor :username
 
   @@all = []
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
+    # - `Viewer.all`
+    #   - returns an array of all the Viewer instances that have been initialized
   end
+
+  def username
+  #   - returns the Viewer's username
+    @username
+  end 
+
+
+  def help_viewer_select
+    #helper method that selects viewer == self from Review All array
+    Review.all.select{|v| v.viewer == self}
+  end
+
+  def reviews
+  #   - returns an array of `Review` instances associated with the `Viewer` instance.
+    help_viewer_select
+  end
+
+  def reviewed_movies
+  #   - returns an array of `Movie` instances reviewed by the `Viewer` instance.
+    help_viewer_select.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+  #   - a `Movie` instance is the only argument
+  end 
   
+  #   - returns `true` if the `Viewer` has reviewed this `Movie` (if there is a `Review` instance that has this `Viewer` and `Movie`), returns `false` otherwise
+  # - `Viewer#rate_movie(movie, rating)`
+  #   - a `Movie` instance and a rating (number) are passed in as arguments
+  #   - if the `Viewer` instance and the passed `Movie` instance are _not_ already associated, this method should create a new `Review` instance
+  #   - if this `Viewer` has already reviewed this `Movie`, assigns the new rating to the existing `Review` instance
+    
 end
